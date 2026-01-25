fa992b201a91dbd2ca770de2cb72eb833bc55623
Tushar Gurjar <tushargurjar202@gmail.com>
2026-01-25 16:52:58 +0530

    Added Vix score to determine market volatility and fixed news_aggregator function call on line 19

--- backend/intelligence_service/app/api/endpoints.py
+++ backend/intelligence_service/app/api/endpoints.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+import yfinance
 from app.services.news_fetcher import NewsAggregator
 from app.ml_engine.sentiment_analyzer import SentimentEngine
 from app.ml_engine.regime_detector import RegimeDetector
@@ -9,13 +10,13 @@ sentiment_engine = SentimentEngine()
 regime_detector = RegimeDetector()
 news_aggregator = NewsAggregator() # Assumes you saved the scraper code from previous turn
 
-@router.post("/refresh-intelligence", response_model=RegimeSignal)
+@router.get("/refresh-intelligence") # response_model=RegimeSignal)
 async def refresh_market_intelligence():
     """
     Main heartbeat: Scrapes news -> Analyzes Sentiment -> Determines Regime
     """
     # 1. Fetch News
-    articles = news_aggregator.get_macro_data()
+    articles = news_aggregator.get_macro_market_news()
     if not articles:
         raise HTTPException(status_code=503, detail="News source unavailable")
     
@@ -28,7 +29,14 @@ async def refresh_market_intelligence():
     
     avg_sentiment = total_score / len(articles) if articles else 0
     
-    # 3. Detect Regime (Mocking volatility as 1.5 for now)
-    decision = regime_detector.detect_regime(market_volatility=1.5, sentiment_score=avg_sentiment)
+    # 3. Detect Regime
+    vix = yfinance.Ticker("INDIAVIX.NS")
+    data = vix.history(period='1d')
+    current_vix = 15 # avg value if data is empty
+    if not data.empty:
+        current_vix = data['Close'].iloc[-1]
+    round(current_vix)
+    current_vix /= 10 # 25 => 2.5 making it between 0-3 
+    decision = regime_detector.detect_regime(market_volatility=current_vix, sentiment_score=avg_sentiment)
     
     return decision
